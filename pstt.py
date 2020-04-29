769d5f8308ec030701dc2c73ad78752a5c0f2891
Erick Cruz <fis9.81@gmail.com>
2020-04-28 21:57:07 -0400

    añadi un if para saber el amyor y menor de 10

--- pstt.py
+++ pstt.py
@@ -1,3 +1,8 @@
 print("hola que hace")
-x=input("introduse un nuemro pai")
-print("{} este es el numero q metiste".format(x))
+c=int(input("introduse un nuemro pai"))
+print("{} este es el numero q metiste".format(c))
+if c<10:
+	print("{} es menor q 10".format(c))
+	pass
+else:
+	print("{} es mayor que 10".format(c))
